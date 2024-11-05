b44a1db7142e2ede501dcecbd9a8733e937f90de
Wesley Camargo <wesley.camargo@TCSNLPS.onmicrosoft.com>
2024-11-05 11:06:23 +0100

    moved template directory

--- 01-templates/workloads/platform/00-TEMPLATE-COPY-ME-design-area/CHANGE-ME-workload/scripts/Agent_ListDirectoryItems.ps1
+++
@@ -1,8 +0,0 @@
-param (
-    [Parameter()]
-    [string]$Directory
-)
-
-# Get all environment variables
-Get-ChildItem -Path $Directory
-
--- 01-templates/workloads/platform/00-TEMPLATE-COPY-ME-design-area/CHANGE-ME-workload/scripts/Agent_ListEnvironmentVariables.ps1
+++
@@ -1,7 +0,0 @@
-# Get all environment variables
-$envVars = Get-ChildItem -Path Env:
-
-# Display the environment variables
-foreach ($envVar in $envVars) {
-    Write-Output "$($envVar.Name) = $($envVar.Value)"
-}
